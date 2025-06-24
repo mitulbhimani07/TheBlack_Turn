019836123f4e4c7af9a8dcbdb94f5505229a2038
foram <forampaneliya1204@gmail.com>
2025-06-24 13:47:50 +0530

    add blog

--- Black-turn-frontedside/src/Header_Footer/Footer.jsx
+++ Black-turn-frontedside/src/Header_Footer/Footer.jsx
@@ -75,7 +75,7 @@ function Footer() {
               <li><a href="#" className="hover:text-[#005f73]">Instagram Stories</a></li>
               <li><a href="#" className="hover:text-[#005f73]">FB - Community</a></li>
               <li><a href="#" className="hover:text-[#005f73]">Contact Us</a></li>
-              <li><a href="#" className="hover:text-[#005f73]">Blog</a></li>
+              <li><Link to="/blog" className="hover:text-[#005f73]">Blog</Link></li>
             </ul>
           </div>
 
