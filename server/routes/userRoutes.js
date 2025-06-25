8b184dd8b42d7fcc8c11dce5ed6ac98ed68f2bec
forampaneliya <forampaneliya1204@gmail.com>
2025-06-25 15:29:32 +0530

    add

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,6 +1,6 @@
 const express=require("express")
 const userRoutes=express.Router()
-
+const userController=require("../controller/userController")
 userRoutes.post("/signup",userController.signup)
 
 
