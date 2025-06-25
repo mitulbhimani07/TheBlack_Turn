c50fd9c0d6648aa28eac68b1a0dfd29a4b832fb0
forampaneliya <forampaneliya1204@gmail.com>
2025-06-25 16:14:36 +0530

    create google signin

--- server/controller/userController.js
+++ server/controller/userController.js
@@ -78,6 +78,37 @@ module.exports.googleSignup = async (req, res) => {
     }
 };
 
+module.exports.googleSignin = async (req, res) => {
+    try {
+        const { email } = req.body;
+
+        if (!email) {
+            return res.status(400).json({ message: "Email is required" });
+        }
+
+        const user = await UserModel.findOne({ email });
+
+        if (!user) {
+            return res.status(404).json({ message: "User not found. Please sign up first." });
+        }
+
+        const userResponse = user.toObject();
+        delete userResponse.password;
+        delete userResponse.conPassword;
+
+        return res.status(200).json({
+            message: "Signin successful",
+            data: userResponse
+        });
+
+    } catch (error) {
+        console.error("Google Signin Error:", error);
+        return res.status(500).json({ message: "Internal server error" });
+    }
+};
+
+
+
 
 
 
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -3,6 +3,8 @@ const userRoutes=express.Router()
 const userController=require("../controller/userController")
 userRoutes.post("/signup",userController.signup)
 userRoutes.post("/googleSignup",userController.googleSignup)
+userRoutes.post("/googleSignin",userController.googleSignin)
+
 
 
 module.exports = userRoutes;
