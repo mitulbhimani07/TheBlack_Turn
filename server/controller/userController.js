acd7c2167cc2d29994535f7e35937b0e9d7bb707
forampaneliya <forampaneliya1204@gmail.com>
2025-06-25 15:53:23 +0530

    add google signup

--- server/controller/userController.js
+++ server/controller/userController.js
@@ -10,3 +10,49 @@ module.exports.signup = async (req, res) => {
     }
 };
 
+
+module.exports.googleSignup = async (req, res) => {
+    try {
+        const { email, username = "" } = req.body;
+
+        if (!email) {
+            return res.status(400).json({ message: "Email is required" });
+        }
+
+        let user = await UserModel.findOne({ email });
+
+        if (!user) {
+            const newUser = await UserModel.create({
+                email,
+                username,
+                password: "",       
+                conPassword: ""     
+            });
+
+            const userResponse = newUser.toObject();
+            delete userResponse.password;
+            delete userResponse.conPassword;
+
+            return res.status(201).json({
+                message: "Google user created successfully",
+                data: userResponse
+            });
+        } else {
+            const userResponse = user.toObject();
+            delete userResponse.password;
+            delete userResponse.conPassword;
+
+            return res.status(200).json({
+                message: "User already exists",
+                data: userResponse
+            });
+        }
+
+    } catch (error) {
+        console.error("Google Signup Error:", error);
+        return res.status(500).json({ message: "Internal server error" });
+    }
+};
+
+
+
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -2,6 +2,7 @@ const express=require("express")
 const userRoutes=express.Router()
 const userController=require("../controller/userController")
 userRoutes.post("/signup",userController.signup)
+userRoutes.post("/googleSignup",userController.googleSignup)
 
 
 module.exports = userRoutes;
